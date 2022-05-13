8c9f04cf606f4075c46e360e8965747fff440a50
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-13 23:49:16 +0300

    14

--- V2/Applic.java
+++ V2/Applic.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class Applic {
 
     public static void main(String...args){
 
 
-        SpringApplication.run(App.class,args);
+        SpringApplication.run(Applic.class,args);
 
 
 
