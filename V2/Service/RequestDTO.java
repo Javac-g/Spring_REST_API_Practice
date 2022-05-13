2450efadf4d3243f94bd3b9ab564977c4ce023a4
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-13 21:14:01 +0300

    3

--- V2/Service/RequestDTO.java
+++ V2/Service/RequestDTO.java
@@ -1,9 +1,25 @@
 package com.MaksDenysov.V2.Service;
 
 public class RequestDTO {
+
     String name;
     Integer age;
 
+    public String getName() {
 
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
 }
