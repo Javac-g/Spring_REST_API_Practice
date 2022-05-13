bce155cf962db375ffa88e94ffe5f5edb2137cfd
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-13 22:08:39 +0300

    9

--- V2/Service/Serv.java
+++ V2/Service/Serv.java
@@ -81,6 +81,27 @@ public class Serv {
 
     }
 
+    public Integer delete(Integer id){
 
+        int indextoremove = -1;
+
+        for (int i = 0 ; i < datalist.size(); i++){
+
+            if(datalist.get(i).getId().equals(id)){
+
+                indextoremove = i;
+
+            }
+
+        }
+        if(indextoremove != -1){
+
+            datalist.remove(indextoremove);
+
+        }
+        return indextoremove;
+
+
+    }
 
 }
