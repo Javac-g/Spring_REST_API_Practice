35e6aedb1a1d663c87b355832966f62c72bab916
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-13 21:19:51 +0300

    5

--- V2/Service/Serv.java
+++ V2/Service/Serv.java
@@ -1,4 +1,31 @@
 package com.MaksDenysov.V2.Service;
 
+import com.MaksDenysov.V2.Controller.ResponseDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Serv {
+
+    List<ResponseDTO> datalist = new ArrayList<>();
+
+
+    public void create(RequestDTO requestDTO){
+
+        ResponseDTO user = new ResponseDTO();
+
+        user.setName(requestDTO.getName());
+
+        user.setAge(requestDTO.getAge());
+
+        user.setPet(requestDTO.getPet());
+
+        datalist.add(user);
+
+
+
+    }
+
+
+
 }
