cc17ad0d9c550bb6c70dcc51ecea20be3414386e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-14 20:50:48 +0300

    3

--- V2/Config/SpringConfigurationFactory.java
+++ V2/Config/SpringConfigurationFactory.java
@@ -3,18 +3,19 @@ package com.MaksDenysov.V2.Config;
 
 import com.MaksDenysov.V2.Controller.Contr;
 import com.MaksDenysov.V2.Service.Serv;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfigurationFactory {
 
-
+    @Bean
     public Contr controllerBean(){
 
         return new Contr();
 
     }
-
+    @Bean
     public Serv serviceBean(){
 
         return new Serv();
