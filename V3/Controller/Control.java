13bd0aff53952207a62eb7f69f7e77667630c606
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-15 22:33:52 +0300

    9

---
+++ V3/Controller/Control.java
@@ -0,0 +1,29 @@
+package com.MaksDenysov.V3.Controller;
+
+
+import com.MaksDenysov.V3.Service.RequestDTO;
+import com.MaksDenysov.V3.Service.Serv;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/person")
+public class Control {
+
+    private Serv service;
+
+    @PostMapping(value = "/a" , produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO addData(@RequestBody RequestDTO requestDTO){
+
+        return service.create(requestDTO);
+    }
+
+
+
+
+
+
+}
--- V3/Controller/Controller.java
+++
@@ -1,4 +0,0 @@
-package com.MaksDenysov.V3.Controller;
-
-public class Controller {
-}
