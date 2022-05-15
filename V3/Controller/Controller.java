8b576e2c427cd824e9bad5162bb1599c098fbbd7
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-15 22:14:42 +0300

    5

---
+++ V3/Controller/Controller.java
@@ -0,0 +1,4 @@
+package com.MaksDenysov.V3.Controller;
+
+public class Controller {
+}
--- V3/Service/Serv.java
+++ V3/Service/Serv.java
@@ -1,7 +1,13 @@
 package com.MaksDenysov.V3.Service;
 
+import com.MaksDenysov.V3.Controller.ResponseDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Serv {
 
+    List<ResponseDTO> datalist = new ArrayList<>();
 
     public String print(String msg){
 
@@ -9,7 +15,21 @@ public class Serv {
 
     }
 
-    //public
+    public ResponseDTO create(RequestDTO requestDTO){
+
+        ResponseDTO responseDTO = new ResponseDTO();
+
+        responseDTO.setName(requestDTO.getName());
+
+        responseDTO.setId(requestDTO.getId());
+
+        responseDTO.setPet(requestDTO.getPet());
+
+        datalist.add(responseDTO);
+
+        return  responseDTO;
+
+    }
 
 
 
