7b36b6536efd53dde1eb88d32d3918e66c830080
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-16 21:55:28 +0300

    Skill

---
+++ V4/Controller/Skill.java
@@ -0,0 +1,13 @@
+package com.MaksDenysov.V4.Controller;
+
+public class Skill {
+    String backend;
+
+    public String getBackend() {
+        return backend;
+    }
+
+    public void setBackend(String backend) {
+        this.backend = backend;
+    }
+}
