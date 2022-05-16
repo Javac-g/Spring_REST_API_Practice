fdbf06dc1eaa04074aa3956e00b9f9adf14db27f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-16 20:16:33 +0300

    add

--- V4/Config/SpringConfigurationFactory.java
+++ V4/Config/SpringConfigurationFactory.java
@@ -1,7 +1,29 @@
 package com.MaksDenysov.V4.Config;
 
+import com.MaksDenysov.V4.Controller.Contr;
+import com.MaksDenysov.V4.Service.Serv;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfigurationFactory {
+
+    @Bean
+    Contr controllerBean(){
+
+        return new Contr(serviceBean());
+
+    }
+
+    @Bean
+    Serv serviceBean(){
+
+        return new Serv();
+
+    }
+
+
+
+
+
 }
--- V4/Controller/Contr.java
+++ V4/Controller/Contr.java
@@ -1,4 +1,60 @@
 package com.MaksDenysov.V4.Controller;
 
+import com.MaksDenysov.V4.Service.ResponseDTO;
+import com.MaksDenysov.V4.Service.Serv;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/V4")
 public class Contr {
+
+    public Contr(Serv serv) {
+        this.serv = serv;
+    }
+
+    private Serv serv;
+
+    @PostMapping(value = "/a",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO add(@RequestBody RequestDTO requestDTO){
+
+        return   serv.create(requestDTO);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
