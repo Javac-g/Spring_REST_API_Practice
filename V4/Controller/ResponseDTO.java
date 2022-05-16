2a3de56627cf1ff24add7b293179c1e430400aa0
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-16 18:04:49 +0300

    ResponseDTO

---
+++ V4/Controller/ResponseDTO.java
@@ -0,0 +1,44 @@
+package com.MaksDenysov.V4.Controller;
+
+public class ResponseDTO {
+
+    String name;
+
+    String position;
+
+    Integer salary;
+
+    Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
