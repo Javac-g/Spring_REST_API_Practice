79dbb7a60ed7f5b1511790f9d99030af2a7ba02a
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-16 21:57:17 +0300

    SKILL ENUM

--- V4/Controller/Skill.java
+++ V4/Controller/Skill.java
@@ -4,10 +4,14 @@ public class Skill {
     String backend;
 
     public String getBackend() {
+
         return backend;
+
     }
 
     public void setBackend(String backend) {
+
         this.backend = backend;
+
     }
 }
---
+++ V4/Service/SkillResultEnum.java
@@ -0,0 +1,10 @@
+package com.MaksDenysov.V4.Service;
+
+public enum SkillResultEnum {
+
+    GOOD,
+    NORMAL,
+    WEAK
+
+
+}
