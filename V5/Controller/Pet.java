af6ce4fb4a90489de411a21b94f5038d3eefc92d
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-17 23:35:26 +0300

    Pet

---
+++ V5/Controller/Pet.java
@@ -0,0 +1,14 @@
+package com.MaksDenysov.V5.Controller;
+
+public class Pet {
+
+    String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
