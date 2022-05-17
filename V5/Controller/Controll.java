e605bd5532f10c0b0050bb11c19013184ac238d1
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-18 00:16:32 +0300

    add

--- V5/Controller/Controll.java
+++ V5/Controller/Controll.java
@@ -1,7 +1,14 @@
 package com.MaksDenysov.V5.Controller;
 
+import com.MaksDenysov.V5.Service.ResponseDTO;
 import com.MaksDenysov.V5.Service.Services;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/crysalis")
 public class Controll {
 
 
@@ -10,4 +17,15 @@ public class Controll {
     public Controll(Services services) {
         this.services = services;
     }
+
+
+    @PostMapping(value = "/a", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO add(@RequestBody RequestDTO requestDTO){
+
+        return services.create(requestDTO);
+
+    }
+
+
 }
