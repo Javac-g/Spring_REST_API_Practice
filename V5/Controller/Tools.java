77630582b1b6b09c6a64ec1a5c1d539e3dd701d1
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-17 23:30:13 +0300

    Tools

---
+++ V5/Controller/Tools.java
@@ -0,0 +1,24 @@
+package com.MaksDenysov.V5.Controller;
+
+public class Tools {
+
+    String IDE;
+
+    String Lang;
+
+    public String getIDE() {
+        return IDE;
+    }
+
+    public void setIDE(String IDE) {
+        this.IDE = IDE;
+    }
+
+    public String getLang() {
+        return Lang;
+    }
+
+    public void setLang(String lang) {
+        Lang = lang;
+    }
+}
