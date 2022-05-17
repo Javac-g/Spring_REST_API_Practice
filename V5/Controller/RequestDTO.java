cc49b9677f3cdebc9f2a1790e0067895d0c0e583
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-17 22:50:22 +0300

    text logger

--- V5/Controller/RequestDTO.java
+++ V5/Controller/RequestDTO.java
@@ -2,7 +2,34 @@ package com.MaksDenysov.V5.Controller;
 
 public class RequestDTO {
     String name;
+    String position;
+    Integer age;
     Integer id;
+    boolean have_friends;
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public boolean isHave_friends() {
+        return have_friends;
+    }
+
+    public void setHave_friends(boolean have_friends) {
+        this.have_friends = have_friends;
+    }
 
     public String getName() {
         return name;
--- V5/Service/Services.java
+++ V5/Service/Services.java
@@ -1,9 +1,48 @@
 package com.MaksDenysov.V5.Service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Services {
 
+    public String printMessage(String msg){
 
+        return msg;
 
+    }
+
+    public void textLog(ResponseDTO responseDTO){
+
+        byte[] bytes = ("\nName: " + responseDTO.getName() +
+                        "\nPosition: " + responseDTO.getPosition() +
+                        "\nAge: " + responseDTO.getAge() +
+                        "\nId: " + responseDTO.getId()).getBytes();
+
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+
+        try {
+
+            b.write(bytes);
+
+        } catch (IOException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+        try(FileOutputStream F = new FileOutputStream("V5_Text_Log.txt",true)){
+
+            b.writeTo(F);
+
+        } catch (IOException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
 
 
 }
