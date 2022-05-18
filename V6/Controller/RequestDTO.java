810bfe67e8f0b20000e33db981956f9ac2a90b46
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-18 23:31:12 +0300

    9

--- V6/Controller/RequestDTO.java
+++ V6/Controller/RequestDTO.java
@@ -1,10 +1,35 @@
 package com.MaksDenysov.V6.Controller;
 
+import com.MaksDenysov.V6.Services.CarEnum;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RequestDTO {
 
     String name;
 
     String work;
+
+    List<Car> cardata = new ArrayList<>();
+
+    Tool tool;
+
+    public List<Car> getCardata() {
+        return cardata;
+    }
+
+    public void setCardata(List<Car> cardata) {
+        this.cardata = cardata;
+    }
+
+    public Tool getTool() {
+        return tool;
+    }
+
+    public void setTool(Tool tool) {
+        this.tool = tool;
+    }
 
     public String getName() {
         return name;
