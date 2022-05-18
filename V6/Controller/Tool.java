c4ffd11f8d8bd5c0466f5bbc4d4adc17a0ce5ee2
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-18 23:20:43 +0300

    4 toool

--- V6/Controller/Tool.java
+++ V6/Controller/Tool.java
@@ -1,4 +1,14 @@
 package com.MaksDenysov.V6.Controller;
 
 public class Tool {
+
+    String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
