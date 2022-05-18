9bd00e828bc77f2e52ae887f701939c75bfa605d
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-18 23:24:16 +0300

    7

--- V6/Controller/Control.java
+++ V6/Controller/Control.java
@@ -1,4 +1,15 @@
 package com.MaksDenysov.V6.Controller;
 
+import com.MaksDenysov.V6.Services.Services;
+
 public class Control {
+    private Services services;
+    public Control(Services services) {
+
+        this.services = services;
+    }
+
+
+
+
 }
