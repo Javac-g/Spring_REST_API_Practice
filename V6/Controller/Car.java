f11dcc3e3fcabc50ef77f2979ec97463cd6f3c51
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-18 23:22:19 +0300

    6

--- V6/Controller/Car.java
+++ V6/Controller/Car.java
@@ -3,5 +3,11 @@ package com.MaksDenysov.V6.Controller;
 public class Car {
     String brand;
 
+    public String getBrand() {
+        return brand;
+    }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 }
