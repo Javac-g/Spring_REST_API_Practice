3528de283c853b6d4c90963120c6ba1ea1b14615
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-18 23:19:55 +0300

    3 request

--- V6/Controller/RequestDTO.java
+++ V6/Controller/RequestDTO.java
@@ -1,4 +1,24 @@
 package com.MaksDenysov.V6.Controller;
 
 public class RequestDTO {
+
+    String name;
+
+    String work;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
 }
