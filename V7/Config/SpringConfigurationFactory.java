da57258dbd404c1a6a0701032c829f9c77d6e547
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-20 00:04:04 +0300

    1

--- V7/Config/SpringConfigurationFactory.java
+++ V7/Config/SpringConfigurationFactory.java
@@ -1,4 +1,24 @@
 package com.MaksDenysov.V7.Config;
 
+import com.MaksDenysov.V7.Controller.ControLLer;
+import com.MaksDenysov.V7.Service.Services;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpringConfigurationFactory {
+
+    @Bean
+
+   public Services servicesBean(){
+
+        return new Services();
+   }
+
+   @Bean
+   public ControLLer controLLerBean(){
+
+       return new ControLLer(servicesBean());
+
+   }
 }
--- V7/Controller/ControLLer.java
+++ V7/Controller/ControLLer.java
@@ -1,4 +1,13 @@
 package com.MaksDenysov.V7.Controller;
 
+import com.MaksDenysov.V7.Service.Services;
+
 public class ControLLer {
+
+    Services make;
+
+    public ControLLer(Services make) {
+        this.make = make;
+    }
+
 }
