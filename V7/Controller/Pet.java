d3daa23a4af355e6629ae8faa602298bc0b6ca73
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-19 23:56:10 +0300

    17

---
+++ V7/Controller/Pet.java
@@ -0,0 +1,24 @@
+package com.MaksDenysov.V7.Controller;
+
+public class Pet {
+
+    String name;
+
+    Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
