544c7eda047a6f5d9495c24ace303df5db37af29
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-19 23:44:38 +0300

    4

---
+++ V7/App.java
@@ -0,0 +1,17 @@
+package com.MaksDenysov.V7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String...args){
+
+
+        SpringApplication.run(App.class,args);
+
+
+
+    }
+
+}
