60720bc5c4c7378c8479cdb7282e76bacdaa7313
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-20 00:35:10 +0300

    5

--- V7/Service/Services.java
+++ V7/Service/Services.java
@@ -2,6 +2,10 @@ package com.MaksDenysov.V7.Service;
 
 import com.MaksDenysov.V7.Controller.ResponseDTO;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +16,34 @@ public class Services {
     public String print(String msg){
 
         return msg;
+
+    }
+    public void log(String filename,ResponseDTO user){
+
+        byte[] bytes = ("\nName: "+ user.getName() + "\nPosition: " + user.getPosition() + "\nAge: " + user.getAge() + "\nId: " + user.getId()).getBytes();
+
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        try {
+
+            b.write(bytes);
+
+        }catch (IOException e){
+
+            e.printStackTrace();
+        }
+        try(DataOutputStream d = new DataOutputStream( new FileOutputStream(filename+".dat",true));
+
+            FileOutputStream F = new FileOutputStream(filename+".txt",true)){
+
+            b.writeTo(F);
+
+            d.writeUTF("\nName: " + user.getName() + "\nPosition: " + user.getPosition() + "\nAge: " + user.getAge() + "\nId: " + user.getId());
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
 
     }
 
