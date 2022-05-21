2dd8245c235c957f3dedade484a8f5cb7dd4970d
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-22 00:41:11 +0300

    3

--- V8/Controller/RequestDTO.java
+++ V8/Controller/RequestDTO.java
@@ -1,8 +1,31 @@
 package com.MaksDenysov.V8.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RequestDTO {
     String name;
     Integer id;
+
+    List<Pet> petList = new ArrayList<>();
+
+    Tool steck;
+
+    public List<Pet> getPetList() {
+        return petList;
+    }
+
+    public void setPetList(List<Pet> petList) {
+        this.petList = petList;
+    }
+
+    public Tool getSteck() {
+        return steck;
+    }
+
+    public void setSteck(Tool steck) {
+        this.steck = steck;
+    }
 
     public String getName() {
         return name;
--- V8/Controller/Tool.java
+++ V8/Controller/Tool.java
@@ -2,4 +2,22 @@ package com.MaksDenysov.V8.Controller;
 
 public class Tool {
 
+    String back;
+    String front;
+
+    public String getBack() {
+        return back;
+    }
+
+    public void setBack(String back) {
+        this.back = back;
+    }
+
+    public String getFront() {
+        return front;
+    }
+
+    public void setFront(String front) {
+        this.front = front;
+    }
 }
