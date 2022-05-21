e9c41a73af6a2f0cad3532bf0c5515993f65fe0b
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-22 01:05:27 +0300

    h

--- V8/Service/ResponseDTO.java
+++ V8/Service/ResponseDTO.java
@@ -1,9 +1,31 @@
 package com.MaksDenysov.V8.Service;
 
+import com.MaksDenysov.V8.Controller.Tool;
+
 public class ResponseDTO {
 
     String name;
     Integer id;
+
+    Tool steck;
+
+    Pet_Enum pet;
+
+    public Tool getSteck() {
+        return steck;
+    }
+
+    public void setSteck(Tool steck) {
+        this.steck = steck;
+    }
+
+    public Pet_Enum getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet_Enum pet) {
+        this.pet = pet;
+    }
 
     public String getName() {
         return name;
