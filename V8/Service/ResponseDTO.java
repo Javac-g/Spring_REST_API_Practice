88620aec8949540e6c302cf2c8687730556c61c6
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-22 00:12:13 +0300

    1

--- V8/Service/ResponseDTO.java
+++ V8/Service/ResponseDTO.java
@@ -2,5 +2,22 @@ package com.MaksDenysov.V8.Service;
 
 public class ResponseDTO {
 
+    String name;
+    Integer id;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
