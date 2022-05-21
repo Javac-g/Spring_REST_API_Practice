841122fef5ee602d7a95af5b3d382bda59de81f2
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-22 00:08:51 +0300

    17

--- V8/Controller/RequestDTO.java
+++ V8/Controller/RequestDTO.java
@@ -1,4 +1,22 @@
 package com.MaksDenysov.V8.Controller;
 
 public class RequestDTO {
+    String name;
+    Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
--- V8/Service/Services.java
+++ V8/Service/Services.java
@@ -1,7 +1,23 @@
 package com.MaksDenysov.V8.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Services {
 
 
+    List<ResponseDTO> datalist = new ArrayList<>();
 
+
+    public String print(String str){
+
+        return str;
+
+    }
+
+    public void logger(String filename,ResponseDTO user){
+
+        byte[] bytes = ("Name: ").getBytes();
+
+    }
 }
