79110e350e8d989ba56da019b8cf0d9130871ec0
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2022-06-27 22:32:02 +0300

    Update Serv.java

--- V4/Service/Serv.java
+++ V4/Service/Serv.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class Serv {
 
-    List<ResponseDTO> database = new ArrayList<>();
+    List<ResponseDTO> datalist = new ArrayList<>();
 
 
     public String print(String message){
