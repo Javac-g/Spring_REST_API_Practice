2d238908d77ac848de0ee18e3611d1cc01499039
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2023-05-16 23:23:43 +0300

    Update Contr.java

--- V1/Controller/Contr.java
+++ V1/Controller/Contr.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
-@RequestMapping("/myapp")
+@RequestMapping("/mygirstapp")
 @Controller
 public class Contr {
 
